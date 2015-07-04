52ceb809e5cc59a2d8375f2094bc597f41a33254
macodd <github.com@higger.de>
2015-07-04 10:56:30 +0200

    parse strength of offense and parse defense weapons

--- src/main/java/de/higger/fwwikiparser/Main.java
+++ src/main/java/de/higger/fwwikiparser/Main.java
@@ -3,12 +3,11 @@ package de.higger.fwwikiparser;
 import static de.higger.fwwikiparser.helper.PropertieHelper.getPropertieHelperInstance;
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 import de.higger.fwwikiparser.converter.parse_item.ParseItemToSQLExpressionInterpreter;
 import de.higger.fwwikiparser.parser.category.BaseCategoryParser;
-import de.higger.fwwikiparser.parser.category.OffenceCategoryParser;
+import de.higger.fwwikiparser.parser.category.DefenceCategoryParser;
 import de.higger.fwwikiparser.vo.BaseParseItem;
 
 public class Main {
@@ -17,12 +16,8 @@ public class Main {
 		
 //		parseItems(new NPCCategoryParser());
 //		parseItems(new AbilityCategoryParser());
-		parseItems(new OffenceCategoryParser());
-		
-		
-		List<String> sites = new LinkedList<String>();
-		sites.add("/index.php/Doppelaxt");
-		
+//		parseItems(new OffenceCategoryParser());
+		parseItems(new DefenceCategoryParser());
 		
 	}
 
---
+++ src/main/java/de/higger/fwwikiparser/parser/category/DefenceCategoryParser.java
@@ -0,0 +1,45 @@
+package de.higger.fwwikiparser.parser.category;
+
+import java.util.List;
+
+import de.higger.fwwikiparser.helper.PropertieHelper;
+import de.higger.fwwikiparser.vo.DefenceArmVO;
+
+public class DefenceCategoryParser extends BaseWeaponCategoryParser<DefenceArmVO> {
+
+	public class DefenceParseJob extends BaseWeaponParseJob {
+
+		public DefenceParseJob(List<String> sitesToParse) {
+			super(sitesToParse);
+		}
+
+		@Override
+		protected DefenceArmVO createObject() {
+			return new DefenceArmVO();
+		}
+		
+	}
+	
+	@Override
+	protected String getCategoryStart() {
+		return PropertieHelper.getPropertieHelperInstance().getProperty("wiki.parser.category.arms.defence");
+	}
+	
+	@Override
+	protected String[] getIgnoredSubSites() {
+		
+		String[] exclustions = {
+			"/index.php/Verteidigungswaffe"
+			, "/index.php/Verteidigungswaffe/St%C3%A4rkste"
+			, "/index.php/Deutschland-Trikot_(W11)"
+		};
+		
+		return exclustions;
+	}
+	
+	@Override
+	protected ParseSiteJob createParseSiteJob(List<String> allocatedSiteList) {
+		return new DefenceParseJob(allocatedSiteList);
+	}
+
+}
